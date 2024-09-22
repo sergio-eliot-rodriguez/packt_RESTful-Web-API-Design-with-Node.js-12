48968ceb677f8d918496dc34e925e10f25ef5277
Checo <sergio_eliot@hotmail.com>
2024-09-21 21:53:21 -0700

    Testing Node.js Server with SuperTest

--- config/express-server.js
+++ config/express-server.js
@@ -1,6 +1,6 @@
 import express from "express";
 const server = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 //Override listen method
 server.listen = server.listen.bind(server, PORT, () => 
